52f89a4b79103a32f9de7e230a434a5e1af252c4
kimhaemi <imeahmik@gmail.com>
2023-06-19 23:52:49 +0900

    화면 메뉴추가 2

--- src/main/java/kr/or/kimsn/radarsms/controller/SmsController.java
+++ src/main/java/kr/or/kimsn/radarsms/controller/SmsController.java
@@ -1,25 +1,75 @@
 package kr.or.kimsn.radarsms.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import kr.or.kimsn.radarsms.dto.MenuDto;
+import kr.or.kimsn.radarsms.dto.StationDto;
+import kr.or.kimsn.radarsms.service.MenuService;
 
 @Controller
 public class SmsController {
+
+    @Autowired
+    private MenuService menuService;
+    
     //문자 발송
     @GetMapping("/manage/sms_send")
-    public String sms_send(){
+    public String sms_send(ModelMap model) {
+        Map<String, Object> map = new HashMap<>();
+
+        List<MenuDto> menuList = menuService.getMenuList();
+        // table join 필요
+        // SELECT site, data_kind, data_type, recv_condition, apply_time, last_check_time, sms_send, sms_send_activation
+		// 				FROM watchdog.receive_condition; 
+        List<StationDto> stationList = menuService.getStationList();
+
+        map.put("menuList", menuList);
+        map.put("stationList", stationList);
+        
+        model.addAttribute("list", map);
         return "views/manage/sms/sms_send";
     }
 
     //문자 발송 대기 내역
     @GetMapping("/manage/sms_send_result")
-    public String sms_send_result(){
+    public String sms_send_result(ModelMap model) {
+        Map<String, Object> map = new HashMap<>();
+
+        List<MenuDto> menuList = menuService.getMenuList();
+        // table join 필요
+        // SELECT site, data_kind, data_type, recv_condition, apply_time, last_check_time, sms_send, sms_send_activation
+		// 				FROM watchdog.receive_condition; 
+        List<StationDto> stationList = menuService.getStationList();
+
+        map.put("menuList", menuList);
+        map.put("stationList", stationList);
+        
+        model.addAttribute("list", map);
         return "views/manage/sms/sms_send_result";
     }
 
     //문자 발송 기능 ON/OFF 설정
     @GetMapping("/manage/sms_send_onoff")
-    public String sms_send_onoff(){
+    public String sms_send_onoff(ModelMap model) {
+        Map<String, Object> map = new HashMap<>();
+
+        List<MenuDto> menuList = menuService.getMenuList();
+        // table join 필요
+        // SELECT site, data_kind, data_type, recv_condition, apply_time, last_check_time, sms_send, sms_send_activation
+		// 				FROM watchdog.receive_condition; 
+        List<StationDto> stationList = menuService.getStationList();
+
+        map.put("menuList", menuList);
+        map.put("stationList", stationList);
+        
+        model.addAttribute("list", map);
         return "views/manage/sms/sms_send_onoff";
     }
 }
--- src/main/java/kr/or/kimsn/radarsms/controller/SmsGroupController.java
+++ src/main/java/kr/or/kimsn/radarsms/controller/SmsGroupController.java
@@ -1,37 +1,111 @@
 package kr.or.kimsn.radarsms.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import kr.or.kimsn.radarsms.dto.MenuDto;
+import kr.or.kimsn.radarsms.dto.StationDto;
+import kr.or.kimsn.radarsms.service.MenuService;
 
 @Controller
 public class SmsGroupController {
+    
+    @Autowired
+    private MenuService menuService;
+
     //문자 수신 그룹 관리
     @GetMapping("/manage/sms_target_group")
-    public String sms_target_group(){
+    public String sms_target_group(ModelMap model) {
+        Map<String, Object> map = new HashMap<>();
+
+        List<MenuDto> menuList = menuService.getMenuList();
+        // table join 필요
+        // SELECT site, data_kind, data_type, recv_condition, apply_time, last_check_time, sms_send, sms_send_activation
+		// 				FROM watchdog.receive_condition; 
+        List<StationDto> stationList = menuService.getStationList();
+
+        map.put("menuList", menuList);
+        map.put("stationList", stationList);
+        
+        model.addAttribute("list", map);
         return "views/manage/smsGroup/sms_target_group";
     }
 
     //문자 수신 그룹 관리 > [ [1그룹] 관악 백령 광덕 면봉 ] 그룹 멤버 관리
     @GetMapping("/manage/sms_target_group_member")
-    public String sms_target_group_member(){
+    public String sms_target_group_member(ModelMap model) {
+        Map<String, Object> map = new HashMap<>();
+
+        List<MenuDto> menuList = menuService.getMenuList();
+        // table join 필요
+        // SELECT site, data_kind, data_type, recv_condition, apply_time, last_check_time, sms_send, sms_send_activation
+		// 				FROM watchdog.receive_condition; 
+        List<StationDto> stationList = menuService.getStationList();
+
+        map.put("menuList", menuList);
+        map.put("stationList", stationList);
+        
+        model.addAttribute("list", map);
         return "views/manage/smsGroup/sms_target_group_member";
     }
 
     //그룹 관리 > [ [1그룹] 관악 백령 광덕 면봉 ] 그룹 감시 자료 설정
     @GetMapping("/manage/sms_target_group_link")
-    public String sms_target_group_link(){
+    public String sms_target_group_link(ModelMap model) {
+        Map<String, Object> map = new HashMap<>();
+
+        List<MenuDto> menuList = menuService.getMenuList();
+        // table join 필요
+        // SELECT site, data_kind, data_type, recv_condition, apply_time, last_check_time, sms_send, sms_send_activation
+		// 				FROM watchdog.receive_condition; 
+        List<StationDto> stationList = menuService.getStationList();
+
+        map.put("menuList", menuList);
+        map.put("stationList", stationList);
+        
+        model.addAttribute("list", map);
         return "views/manage/smsGroup/sms_target_group_link";
     }
 
     //문자 수신자 관리
     @GetMapping("/manage/sms_target_member")
-    public String sms_target_member(){
+    public String sms_target_member(ModelMap model) {
+        Map<String, Object> map = new HashMap<>();
+
+        List<MenuDto> menuList = menuService.getMenuList();
+        // table join 필요
+        // SELECT site, data_kind, data_type, recv_condition, apply_time, last_check_time, sms_send, sms_send_activation
+		// 				FROM watchdog.receive_condition; 
+        List<StationDto> stationList = menuService.getStationList();
+
+        map.put("menuList", menuList);
+        map.put("stationList", stationList);
+        
+        model.addAttribute("list", map);
         return "views/manage/smsGroup/sms_target_member";
     }
 
     //상시 문자 수신 그룹 관리
     @GetMapping("/manage/sms_target_monitorgroup")
-    public String sms_target_monitorgroup(){
+    public String sms_target_monitorgroup(ModelMap model) {
+        Map<String, Object> map = new HashMap<>();
+
+        List<MenuDto> menuList = menuService.getMenuList();
+        // table join 필요
+        // SELECT site, data_kind, data_type, recv_condition, apply_time, last_check_time, sms_send, sms_send_activation
+		// 				FROM watchdog.receive_condition; 
+        List<StationDto> stationList = menuService.getStationList();
+
+        map.put("menuList", menuList);
+        map.put("stationList", stationList);
+        
+        model.addAttribute("list", map);
         return "views/manage/smsGroup/sms_target_monitorgroup";
     }
 }
