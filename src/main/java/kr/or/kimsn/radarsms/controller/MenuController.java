7201697770799dc39886fa4617ff25d114f256db
kimhaemi <imeahmik@gmail.com>
2023-06-19 23:53:43 +0900

    필요없는 import 제거

--- src/main/java/kr/or/kimsn/radarsms/controller/MenuController.java
+++ src/main/java/kr/or/kimsn/radarsms/controller/MenuController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import kr.or.kimsn.radarsms.dto.MenuDto;
 import kr.or.kimsn.radarsms.dto.StationDto;
--- src/main/java/kr/or/kimsn/radarsms/controller/StationController.java
+++ src/main/java/kr/or/kimsn/radarsms/controller/StationController.java
@@ -5,12 +5,10 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.servlet.ModelAndView;
 
 import kr.or.kimsn.radarsms.dto.MenuDto;
 import kr.or.kimsn.radarsms.dto.StationDto;
