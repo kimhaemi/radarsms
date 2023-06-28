376241d7a9a58488a8cce5c4b42bf29f0538f6e4
김해미 <imeahmik@gmail.com>
2023-06-29 00:11:32 +0900

    native query 어렵다. ㅡㅡ

--- src/main/java/kr/or/kimsn/radarsms/repository/ReceiveConditionRepository.java
+++ src/main/java/kr/or/kimsn/radarsms/repository/ReceiveConditionRepository.java
@@ -13,10 +13,12 @@ public interface ReceiveConditionRepository extends JpaRepository<ReceiveConditi
     List<ReceiveConditionDto> findBySiteAndDataType(String site, String data_type);
 
     @Query(value="select \n" +
-            "T1.data_kind, \n" +
-            "T1.site, T2.name_kr,\n" +
-            "T1.data_type, T3.data_name,\n" +
-            "T1.sms_send_activation\n" +
+            "T1.data_kind as data_kind, \n" +
+            "T1.site as site, \n" +
+            "T2.name_kr as name_kr,\n" +
+            "T1.data_type as data_type, \n"+
+            "T3.data_name as data_name,\n" +
+            "T1.sms_send_activation as sms_send_activation\n" +
             "from receive_condition T1\n" +
             "left outer join station_rdr T2 \n" +
             "on T1.site = T2.site_cd \n" +
@@ -25,7 +27,8 @@ public interface ReceiveConditionRepository extends JpaRepository<ReceiveConditi
             "and T1.data_kind=T3.data_kind \n" +
             "and T1.data_type=T3.data_type \n" +
             "order by \n" +
-            "data_kind desc, name_kr asc, data_type asc")
+            "data_kind desc, name_kr asc, data_type asc",
+            nativeQuery = true)
     List<ReceiveDto> findReceiveConditionStationRdrReceiveSetting();
 
     
